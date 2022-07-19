21a69c25cb4d53bfc05a550c9e72fe0830a5aba1
Spencer Murray <spencer@spalmurray.com>
2022-07-19 13:46:39 -0400

    Add dotenv so we can also run Koa without vercel.

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+import 'dotenv/config';
 import Koa from 'koa';
 import Router from 'koa-router';
 import Mixpanel from 'mixpanel';
 
 export const app = new Koa();
-const router = new Router({ prefix: "/api/mixpanel" });
+const router = new Router({ prefix: "/api/koa" });
 
 // Add this environment variable to a .env file in this directory.
 const mixpanel = Mixpanel.init(process.env.MIXPANEL_PROJECT_TOKEN, {
@@ -14,7 +15,8 @@ router.get('/', async ctx => {
   mixpanel.track('tracking event inside route', {
     distinct_id: '113',
   });
-  ctx.body = "Sent Mixpanel event.";
+  ctx.body = "Sent Mixpanel event using Vercel and Koa.";
 });
 
 app.use(router.routes());
+app.listen(3000);
